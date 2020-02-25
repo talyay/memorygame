d911b0508f652da1b43d9ad24655ac69208453eb
OWNER <OWNER@65-6>
2020-02-25 11:35:27 +0200

    Add project files.

---
+++ MemoryGame/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace MemoryGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(174, 85);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(302, 133);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "להתחלה לחץ ";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 348);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ MemoryGame/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryGame
+{
+    public partial class Form1 : Form
+    {// משתנים גלובלים
+        Random rnd = new Random();
+        PictureBox[] allp;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+         private void Form1_Load(object sender, EventArgs e)//לעשות כפתור רק כשלוחצים עליו זה מפעיל את שלושתם
+          {
+        // buildpicture(40);
+        //  shuffle();
+        //     showboard(8,5);
+  }//
+        private void shuffle()
+        {
+            int num;
+            PictureBox tmp;
+            for (int i = 0; i < allp.Length; i++)
+            {
+                num = rnd.Next(0, allp.Length);
+                tmp = allp[i];
+                allp[i] = allp[num];
+                allp[num] = tmp;
+            }
+        }
+        private void showboard(int row, int col)
+        {
+            int x = 100, y = 100;
+            int place = 0;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    allp[place].Location = new Point(x, y);
+                    x += 85;
+                    Controls.Add(allp[place++]);
+                }
+                y += 85;
+                x = 100;
+            }
+        }
+        private void buildpicture(int size)
+        {
+            int picnum = 1;
+            allp = new PictureBox[size];
+            for (int i = 0; i < allp.Length - 1; i += 2)
+            {
+                allp[i] = new PictureBox();
+                allp[i].Image = (Image)Properties.Resources.ResourceManager.GetObject("pic" + (picnum));
+                allp[i].Size = new Size(80, 80);
+                allp[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                allp[i].BorderStyle = BorderStyle.FixedSingle;
+                allp[i].Tag = (i + 1) + "";
+                allp[i].Click += allP_Click;
+                allp[i + 1] = new PictureBox();
+                allp[i + 1].Image = (Image)Properties.Resources.ResourceManager.GetObject("pic" + (picnum++));
+                allp[i + 1].Size = new Size(80, 80);
+                allp[i + 1].SizeMode = PictureBoxSizeMode.StretchImage;
+                allp[i + 1].BorderStyle = BorderStyle.FixedSingle;
+                allp[i + 1].Tag = (i + 1) + "";
+                allp[i + 1].Click += allP_Click;
+            }
+        }
+        private void allP_Click(object sender, EventArgs e)
+        {
+           
+        }
+         private void picturebox1_click(object sender , EventArgs e)
+        {
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            buildpicture(40);
+            shuffle();
+            showboard(8, 5);
+            btn1.Hide();
+        }
+    }
+}
+
+
+
